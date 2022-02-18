bd603db8b451ef7cf84a95206baa372374726461
Phenomen <phenomen@pm.me>
2022-02-18 10:38:33 -0600

    Abilities and Skills

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,31 +1,35 @@
 <script>
 	import { localStorageStore } from '@babichjacob/svelte-localstorage/svelte-kit';
 	import { onMount } from 'svelte';
+	import { MoonIcon, SunIcon } from '@rgossiaux/svelte-heroicons/solid';
 
-	import { MoonIcon } from '@rgossiaux/svelte-heroicons/solid';
-
-	const theme = localStorageStore('theme', 'light');
+	const dark = localStorageStore('dark', false);
 
 	function switchTheme() {
-		if ($theme == 'light') {
-			$theme = 'dark';
+		if (!$dark) {
 			document.documentElement.classList.add('dark');
 		} else {
-			$theme = 'light';
 			document.documentElement.classList.remove('dark');
 		}
+		$dark = !$dark;
 	}
 
 	onMount(async () => {
-		if ($theme == 'light') {
-			document.documentElement.classList.remove('dark');
-		} else {
+		if ($dark) {
 			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
 		}
 	});
 </script>
 
 <div class="my-4 flex space-x-2 dark:text-slate-100">
 	<h1 class="flex-1 text-sm font-bold uppercase ">D&D 5e - Справочник на русском языке</h1>
-	<MoonIcon class="h-6 w-6 hover:cursor-pointer" on:click={switchTheme} />
+	<div class="cursor-pointer">
+		{#if $dark}
+			<SunIcon class="h-6 w-6" on:click={switchTheme} />
+		{:else}
+			<MoonIcon class="h-6 w-6" on:click={switchTheme} />
+		{/if}
+	</div>
 </div>
