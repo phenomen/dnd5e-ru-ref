a30a008bb7dc606d0a0a9cd0f7c74e307e9a34ba
Aleksandr Vasilenko <phenomen@proton.me>
2022-08-16 14:48:06 -0500

    Updated footer links. RIP Kosti

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,15 +5,13 @@
 <div class="my-8 flex w-full space-x-2 rounded bg-slate-100 p-4 text-xs dark:bg-slate-800 md:text-sm">
 	<h3 class="font-medium text-slate-600 dark:text-slate-300">Полезные сообщества:</h3>
 	<a class="text-blue-500 underline hover:text-blue-300" href="https://discord.gg/Z2CXFy35WF" target="_blank">Foundry VTT RU</a>
-	<a class="text-blue-500 underline hover:text-blue-300" href="https://vk.com/vecherniyekosti" target="_blank">Вечерние Кости</a>
-	<a class="text-blue-500 underline hover:text-blue-300" href="https://vk.com/noroleplaying" target="_blank">NoRP</a>
+	<a class="text-blue-500 underline hover:text-blue-300" href="https://discord.gg/mkRBmjneJe" target="_blank">Адекватная Ролевая</a>
 	<a class="text-blue-500 underline hover:text-blue-300" href="https://discord.gg/AEj8YGX96m" target="_blank">Подземелья и Подземелья</a>
 </div>
 
 <div class="mt-20 text-xs text-slate-400">
 	<a href="https://github.com/Phenomen/dnd5e-ru-ref" target="_blank" class="flex items-center justify-center">
 		<span class="h-5 w-5"><IconCode /></span>
-
 		<span class="ml-1 font-medium hover:underline">open source by phenomen#1337</span>
 	</a>
 </div>
