3b760c71a1ef302baf8b7fe4a005f021072decd6
Aleksandr Vasilenko <phenomen@proton.me>
2022-08-16 14:53:36 -0500

    Update Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,13 +6,12 @@
 
 <div class="my-4 flex space-x-2 dark:text-slate-100">
 	<h1 class="flex-1 text-sm font-bold uppercase ">D&D 5e - Справочник на русском языке</h1>
-	<div class="flex justify-center">
-		<span use:themetoggle={['light', 'dark']} class="flex cursor-pointer place-items-center">
-			{#if $theme === 'dark'}
-				<span class="h-6 w-6 text-yellow-400"><IconSun /></span>
-			{:else}
-				<span class="h-6 w-6 text-slate-700"><IconMoon /></span>
-			{/if}
-		</span>
+
+	<div use:themetoggle={['light', 'dark']} class="flex cursor-pointer place-items-center">
+		{#if $theme === 'dark'}
+			<span class="h-6 w-6 text-yellow-400"><IconSun /></span>
+		{:else}
+			<span class="h-6 w-6 text-slate-700"><IconMoon /></span>
+		{/if}
 	</div>
 </div>
