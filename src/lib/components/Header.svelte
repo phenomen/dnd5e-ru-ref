7ce589e60798c99da10fccafa0fea8d33b10dbfa
Aleksandr Vasilenko <phenomen@proton.me>
2022-10-20 12:10:35 -0500

    Removed svu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,17 +1,29 @@
 <script>
-	import { themetoggle } from 'svu/action';
-	import { theme } from 'svu/client';
 	import { IconSun, IconMoon } from '$lib/components/icons';
+
+	let theme = 'light';
+
+	function themeToggle() {
+		if (theme != 'dark') {
+			document.documentElement.classList.add('dark');
+			theme = 'dark';
+		} else {
+			document.documentElement.classList.remove('dark');
+			theme = 'light';
+		}
+	}
 </script>
 
 <div class="my-4 flex space-x-2 dark:text-slate-100">
 	<h1 class="flex-1 text-sm font-bold uppercase ">D&D 5e - Справочник на русском языке</h1>
-
-	<div use:themetoggle={['light', 'dark']} class="flex cursor-pointer place-items-center">
-		{#if $theme === 'dark'}
+	<button
+		class="flex cursor-pointer place-items-center"
+		on:click={() => themeToggle()}
+	>
+		{#if theme === 'dark'}
 			<span class="h-6 w-6 text-yellow-400"><IconSun /></span>
 		{:else}
 			<span class="h-6 w-6 text-slate-700"><IconMoon /></span>
 		{/if}
-	</div>
+	</button>
 </div>
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: ['svu', 'svu/*'],
-	},
-	ssr: {
-		noExternal: ['svu', 'svu/*'],
-	},
 };
 
 export default config;
