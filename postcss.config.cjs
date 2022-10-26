de272e2637c42b24372cf6bc5aa157e9adec059c
Aleksandr Vasilenko <phenomen@proton.me>
2022-10-26 12:25:06 -0500

    Universal adapter

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,5 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
-const mode = process.env.NODE_ENV;
 
 const config = {
 	plugins: [tailwindcss(), autoprefixer()],
