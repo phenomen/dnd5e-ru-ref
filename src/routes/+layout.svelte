b1cbbff924ede1e8ffbe8cd12fc836497bab8698
Aleksandr Vasilenko <phenomen@proton.me>
2022-11-03 10:22:24 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
 	import { inject } from '@vercel/analytics';
-
+	import { onMount } from 'svelte';
 	import '../app.css';
 
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
-	inject();
+	onMount(async () => {
+		inject();
+	});
 </script>
 
 <div class="mx-auto max-w-7xl p-2 sm:p-4 lg:p-6">
