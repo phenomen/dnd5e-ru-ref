c8dbfd400ddccfe170ba31f45b9086fc95b55e81
Aleksandr Vasilenko <phenomen@proton.me>
2023-04-05 11:37:33 -0500

    Save theme in localstorage

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,5 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
 	import { storage } from 'svelte-legos';
 	import { writable } from 'svelte/store';
 	import { toggleAll } from '$lib/components/store';
@@ -10,25 +11,23 @@
 
 	const theme = storage(writable(false), 'theme');
 
-	function themeToggle() {
-		if ($theme) {
+	function setTheme(theme: boolean) {
+		if (theme) {
 			document.documentElement.classList.add('dark');
-			$theme = false;
 		} else {
 			document.documentElement.classList.remove('dark');
-			$theme = true;
 		}
 	}
 
-	function disclosureToggle() {
-		$toggleAll === true ? ($toggleAll = false) : ($toggleAll = true);
-	}
+	onMount(async () => {
+		setTheme($theme);
+	});
 </script>
 
 <div class="my-4 flex items-center space-x-2 dark:text-slate-100 sm:space-x-4">
 	<h1 class="flex-1 text-sm font-bold uppercase">D&D 5e - Справочник на русском языке</h1>
 
-	<button class="flex cursor-pointer" on:click={() => disclosureToggle()}>
+	<button class="flex cursor-pointer" on:click={() => ($toggleAll = !$toggleAll)}>
 		{#if $toggleAll}
 			<LucideChevronsUpDown class="h-6 w-6" />
 		{:else}
@@ -36,11 +35,11 @@
 		{/if}
 	</button>
 
-	<button class="flex cursor-pointer" on:click={() => themeToggle()}>
+	<button class="flex cursor-pointer" on:click={() => (($theme = !$theme), setTheme($theme))}>
 		{#if $theme}
-			<LucideMoon class="h-6 w-6 text-slate-700" />
+			<LucideMoon class="h-6 w-6 text-slate-100" />
 		{:else}
-			<LucideSun class="h-6 w-6 text-slate-100 " />
+			<LucideSun class="h-6 w-6 text-slate-700 " />
 		{/if}
 	</button>
 </div>
