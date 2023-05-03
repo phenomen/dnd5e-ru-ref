5ebd5253a09941da0f01407f3fac117b2b1c685c
Aleksandr Vasilenko <phenomen@proton.me>
2023-05-03 10:16:22 -0500

    Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script>
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
 	import '../app.css';
 	import '@fontsource/dm-sans';
+
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="mx-auto max-w-7xl p-2 md:p-4">
